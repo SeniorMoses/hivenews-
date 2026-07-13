06d78393c150b6477eaa1b4dede72c90208fc217
SeniorMoses <mosessenior11@gmail.com>
2026-07-13 04:41:34 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from database import engine, Base
+from routers import auth_router, news_router
+
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(auth_router.router)
+app.include_router(news_router.router)
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI server running 🚀"}
